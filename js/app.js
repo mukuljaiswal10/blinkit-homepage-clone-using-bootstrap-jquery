a0c2435201f084b306e052d96e07b99d7504fa53
mukuljaiswal <mukuljaiswal282@gmail.com>
2025-11-06 16:36:13 +0530

    first-commit

---
+++ js/app.js
@@ -0,0 +1,192 @@
+// banner-list-slider
+$('#banner-list-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// end 
+
+// item-list-slider
+$('#item-list-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3.5
+        },
+        600:{
+            items:6.5
+        },
+        1000:{
+            items:10.5
+        }
+    }
+})
+// end
+
+// item-list-slider
+$('#item-list-slider-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3.5
+        },
+        600:{
+            items:6.5
+        },
+        1000:{
+            items:10.5
+        }
+    }
+})
+// end
+
+// daily-item-slider
+$('#daily-item-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
+
+// rolling-paper-slider
+$('#rolling-paper-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
+
+// snack-slider
+$('#snack-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
+
+// hookah-slider
+$('#hookah-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
+
+// mouth-freshener-slider
+$('#mouth-freshener-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
+
+// cold-drink-slider
+$('#cold-drink-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
+
+// candies-slider
+$('#candies-slider').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+// end
